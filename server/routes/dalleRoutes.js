820ba0b2ec72ce62287038a170d5730188a9a7a9
huntermacias <supercodersf@gmail.com>
2023-01-21 05:23:16 -0800

    updated dalle route file

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,43 +1,38 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import { Configuration, OpenAIApi } from "openai";
-
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
- 
-const router = express.Router(); 
+
+const router = express.Router();
 
 const configuration = new Configuration({
-	organization: "org-qGvoFLKAazbnCMENvJsYWw7t",
-	apiKey: process.env.OPENAI_API_KEY,
-})
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
 
 router.route('/').get((req, res) => {
-	res.send('hello from dalle')
+  res.status(200).json({ message: 'Hello from DALL-E!' });
 });
 
 router.route('/').post(async (req, res) => {
-	try {
-		const { prompt } = req.body;
+  try {
+    const { prompt } = req.body;
 
-		const aiResponse = await openai.createImage({
-			prompt,
-			n: 1,  
-			size: '1024x1024',
-			response_format: 'b64_json',
-		})
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
 
-		const image = aiResponse.data.data[0].b64_json;
+    const image = aiResponse.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+  }
+});
 
-		// sending image to front end
-		res.status(200).json({ photo: image })
-
-	} catch (error) {
-		console.log(error)
-		res.status(500).send(error?.response.data.error.message);
-	}
-})
-
-export default router; 
+export default router;
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,57 +1,43 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import { v2 as cloudinary } from 'cloudinary'
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
 
-import Post from '../mongodb/models/post.js'
+import Post from '../mongodb/models/post.js';
 
 dotenv.config();
 
-const router = express.Router(); 
+const router = express.Router();
 
-// uploading images to cloudinary
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-//  GET ALL POSTS
-router.route("/").get(async (req, res) => {
+router.route('/').get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+  }
+});
 
-	try {
-		const posts = await Post.find({});
+router.route('/').post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
 
-		res.status(200).json({ success: true, data: posts })
-	} catch(error) {
-		res.status(500).json({ success: false, message: error })
-	}
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
 
-})
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
+  }
+});
 
-
-// CREATE A POST
-router.route("/").post(async (req, res) => {
-	try {
-		const { name, prompt, photo } = req.body; 
-
-		// upload image to cloudinary 
-		const photoUrl = await cloudinary.uploader.upload(photo); 
-
-		// create instance of post
-		const newPost = await Post.create({
-			name, 
-			prompt, 
-			photo: photoUrl.url, 
-		})
-	
-		res.status(201).json({ success: true, data: newPost })
-
-	} catch (error) {
-		res.status(500).json({ success: false, message: error })
-	}
-	
-
-})
-
-
-export default router; 
+export default router;
